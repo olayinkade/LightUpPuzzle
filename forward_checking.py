caa66a049b275e19a45b138fae8fe4c9e8187551
thanhtran18 <tranct@myumanitoba.ca>
2020-01-24 10:36:22 -0600

    add find most constrained cell method

--- forward_checking.py
+++ forward_checking.py
@@ -1,7 +1,10 @@
 from typing import List
+import random
+
+wall_values = ['0', '1', '2', '3', '4']
 
 
-def check_edge_corner(puzzle: List[List[int]], r: int, c: int) -> int:
+def check_edge_corner(puzzle: List[List[str]], r: int, c: int) -> int:
     status = 0
     if r == 0 or r == len(puzzle) - 1:  # edge
         status += 1
@@ -10,7 +13,7 @@ def check_edge_corner(puzzle: List[List[int]], r: int, c: int) -> int:
     return status
 
 
-def get_total_potential_adjacent(puzzle: List[List[int]], r: int, c: int):
+def get_total_potential_adjacent(puzzle: List[List[str]], r: int, c: int) -> int:
     rows, cols, count = len(puzzle), len(puzzle[0]), 0
 
     if r > 0 and isinstance(puzzle[r-1][c], int) and puzzle[r-1][c] >= 2:
@@ -24,3 +27,93 @@ def get_total_potential_adjacent(puzzle: List[List[int]], r: int, c: int):
     return count
 
 
+def forward_checking(puzzle: List[List[int]], domain, non_assigned_cells, heuristic) -> str:
+    num_nodes = 0
+    result = ""
+    if num_nodes % 10000 == 0:
+        print(num_nodes)
+    if num_nodes == 5000000:
+        result = 'Too many nodes. Timeout'
+
+
+def light_map_up(puzzle: List[List[str]]):
+    for r in range(len(puzzle)):
+        for c in range(len(puzzle[0])):
+            if puzzle[r][c] == 'b':
+                k = 1
+                while r - k >= 0 and (puzzle[r-k][c] == '_' or puzzle[r - k][c] == '*'):
+                    puzzle[r-k][c] = '*'
+                    k += 1
+                k = 1
+                while r + k < len(puzzle) and (puzzle[r + k][c] == '_' or puzzle[r + k][c] == '*'):
+                    puzzle[r + k][c] = '*'
+                    k += 1
+                k = 1
+                while c - k >= 0 and (puzzle[r][c - k] == '_' or puzzle[r][c - k] == '*'):
+                    puzzle[r][c - k] = '*'
+                    k += 1
+                k = 1
+                while c + k < len(puzzle[r]) and (puzzle[r][c + k] == '_' or puzzle[r][c + k] == '*'):
+                    puzzle[r][c + k] = '*'
+                    k += 1
+
+
+def count_walls_around(puzzle: List[List[str]], r: int, c: int) -> int:
+    num_walls = 0
+    if r > 0 and puzzle[r-1][c] in wall_values:
+        num_walls += int(int(puzzle[r-1][c])/2 + 1)
+    if r < len(puzzle)-1 and puzzle[r+1][c] in wall_values:
+        num_walls += int(int(puzzle[r+1][c])/2 + 1)
+    if c > 0 and puzzle[r][c-1] in wall_values:
+        num_walls += int(int(puzzle[r][c-1])/2 + 1)
+    if c < len(puzzle[0])-1 and puzzle[r][c+1] in wall_values:
+        num_walls += int(int(puzzle[r][c+1])/2 + 1)
+    return num_walls
+
+
+def count_adjacent_lit_cells(puzzle: List[List[str]], r, c) -> int:
+    count = 0
+    if r > 0 and puzzle[r-1][c] == '*':
+        count += 1
+    if r < len(puzzle)-1 and puzzle[r+1][c] == '*':
+        count += 1
+    if c > 0 and puzzle[r][c-1] == '*':
+        count += 1
+    if c < len(puzzle[0])-1 and puzzle[r][c+1] == '*':
+        count += 1
+    return count
+
+
+def is_map_lit_up_and_clean_map(puzzle: List[List[str]]) -> bool:
+    lit_up = True
+    for r in range(len(puzzle)):
+        for c in range(len(puzzle[r])):
+            if puzzle[r][c] == '_':  # the solution is not valid
+                lit_up = False
+            elif puzzle[r][c] == '*':
+                puzzle[r][c] = '_'
+    return lit_up
+
+
+def find_most_constrained(puzzle: List[List[str]], non_assigned: List[List[int]]) -> int:
+    curr_most_constrained = (-1, -1)
+    light_map_up(puzzle)
+
+    for cell in non_assigned:
+        r = cell[0]
+        c = cell[1]
+
+        num_walls = count_walls_around(puzzle, r, c)
+        # check to see if a cell is in an edge, or corner
+        location = check_edge_corner(puzzle, r, c)
+        adj_lit_cells = count_adjacent_lit_cells(puzzle, r, c)
+
+        constraints = num_walls + location + adj_lit_cells
+        # randomly pick one to pick if the constraints of this cell is the same as the current most constrained
+        if constraints == curr_most_constrained[0] and random.randint(0, 1) == 0:
+            curr_most_constrained = (constraints, cell)
+        if constraints > curr_most_constrained[0]:
+            curr_most_constrained = (constraints, cell)
+
+    is_map_lit_up_and_clean_map(puzzle)
+    return curr_most_constrained[1]
